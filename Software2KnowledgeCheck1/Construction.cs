59d49937fa55a5a766f94558687c64a19f6f8eff
Hunter Austin <htaust01@gmail.com>
2023-06-16 17:08:34 -0400

    Moved methods from City to new class Construction and made Create method generic

---
+++ Software2KnowledgeCheck1/Construction.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace Software2KnowledgeCheck1
+{
+	internal class Construction
+	{
+		public Construction()
+		{
+		}
+
+        public void CreateApartment(Apartment apartment, City city)
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+            var permit = permitRepo.GetPermit();
+            var zoningApproval = permitRepo.ZoningApproves();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permit, zoningApproval);
+
+            if (buildingWasMade)
+            {
+                city.Buildings.Add(apartment);
+            }
+        }
+
+        public void CreateStructure<T>(T structure, City city) where T : Building
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+            var permit = permitRepo.GetPermit();
+            var zoningApproval = permitRepo.ZoningApproves();
+
+            var buildingWasMade = ConstructBuilding<T>(materialsNeeded, permit, zoningApproval);
+
+            if (buildingWasMade)
+            {
+                city.Buildings.Add(structure);
+            }
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
